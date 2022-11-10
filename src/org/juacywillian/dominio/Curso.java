11d0edcbcc2e2308e37f9735919d3793279a2e4a
Juacy Willian <juacy.willian@gmail.com>
2022-11-10 10:27:36 -0300

    classes curso e mentoria

---
+++ src/org/juacywillian/dominio/Curso.java
@@ -0,0 +1,37 @@
+package org.juacywillian.dominio;
+
+public class Curso extends Conteudo {
+    private int cargaHoraria;
+
+    public Curso() {
+    }
+
+    public Curso(String titulo, String descrição, int cargaHoraria) {
+        setTitle(titulo);
+        setDescription(descrição);
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    @Override
+    double calcularXP() {
+        return XP_PADRAO * this.cargaHoraria;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("""
+                Curso {
+                    titulo = %s
+                    descrição = %s
+                    carga horaria = %d
+                }\n""", getTitle(), getDescription(), cargaHoraria);
+    }
+}
---
+++ src/org/juacywillian/dominio/Mentoria.java
@@ -0,0 +1,40 @@
+package org.juacywillian.dominio;
+
+import java.time.LocalDate;
+
+public class Mentoria extends Conteudo {
+
+    private LocalDate data;
+
+    public Mentoria() {
+    }
+
+    public Mentoria(String titulo, String descição, LocalDate data) {
+        setTitle(titulo);
+        setDescription(descição);
+        this.data = data;
+    }
+
+    @Override
+    double calcularXP() {
+        return XP_PADRAO * 20d;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("""
+                Mentoria {
+                    titulo = %s,
+                    descrição = %s,
+                    data = %s
+                }\n""", getTitle(), getDescription(), data.toString());
+    }
+}
