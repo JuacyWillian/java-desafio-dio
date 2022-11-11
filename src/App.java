d139d9cc5690ff33113c673c25252287310d8079
Juacy Willian <juacy.willian@gmail.com>
2022-11-11 10:52:47 -0300

    adicionando editorconfig ao projeto

--- src/App.java
+++ src/App.java
@@ -39,24 +39,30 @@ public class App {
 
         Aluno camila = new Aluno("Camila");
         camila.inscreverBootcamp(formacaoDevJava);
-        System.out.println("Conteudos inscritos de Camila: " + camila.getConteudosInscritosString() + "\n");
+        System.out.println("Conteudos inscritos de Camila: " +
+                camila.getConteudosInscritosString() + "\n");
         camila.progredir();
         camila.progredir();
         System.out.println("");
-        System.out.println("Conteudos inscritos de Camila: " + camila.getConteudosInscritosString() + "\n");
-        System.out.println("Conteudos concluidos de Camila: " + camila.getConteudosConcluidosString() + "\n");
+        System.out.println("Conteudos inscritos de Camila: " +
+                camila.getConteudosInscritosString() + "\n");
+        System.out.println("Conteudos concluidos de Camila: " +
+                camila.getConteudosConcluidosString() + "\n");
         System.out.println("XP: " + camila.calcularTotalXP() + "\n");
 
         System.out.println("\n\n +++ \n\n");
 
         Aluno joao = new Aluno("Joao");
         joao.inscreverBootcamp(formacaoDevJava);
-        System.out.println("Conteudos inscritos de João: " + joao.getConteudosInscritosString() + "\n");
+        System.out.println("Conteudos inscritos de João: " +
+                joao.getConteudosInscritosString() + "\n");
         joao.progredir();
         joao.progredir();
         System.out.println("");
-        System.out.println("Conteudos inscritos de João: " + joao.getConteudosInscritosString() + "\n");
-        System.out.println("Conteudos concluidos de João: " + joao.getConteudosConcluidosString() + "\n");
+        System.out.println("Conteudos inscritos de João: " +
+                joao.getConteudosInscritosString() + "\n");
+        System.out.println("Conteudos concluidos de João: " +
+                joao.getConteudosConcluidosString() + "\n");
         System.out.println("XP: " + joao.calcularTotalXP() + "\n");
 
     }
